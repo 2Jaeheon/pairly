3fcb2285a2c9d388bee05080011a973ff04dc990
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-11-13 02:54:55 +0900

    feat(common): 클라이언트용 공통 유틸 및 예외 정의
    클라이언트용 유틸리티 클래스와 예외 처리를 위한 클래스 구축
    
    세부 사항:
    - 뷰 렌더링을 위한 AnsiConsole 유틸리티 추가
    - 파서가 사용할 ErrorMessage 상수 및 InvalidCommandException 정의

--- src/main/java/pairly/common/exception/InvalidCommandException.java
+++ src/main/java/pairly/common/exception/InvalidCommandException.java
@@ -1,4 +1,11 @@
 package pairly.common.exception;
 
-public class InvalidCommandException {
+public class InvalidCommandException extends IllegalArgumentException {
+    public InvalidCommandException(String message) {
+        super(message);
+    }
+
+    public InvalidCommandException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

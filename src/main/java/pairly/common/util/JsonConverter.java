554c2b929a3b8cea65939079534fd09792e2d9f0
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-11-16 10:52:31 +0900

    feat(util): 직렬화 및 역직렬화 로직 구현
    소켓 통신을 위해서 필요한 JSON 직렬화 및 역직렬화 로직을 구현
    
    세부 사항:
    - gradle에 gson 라이브러리 추가
    - Gson 라이브러리를 이용하여 직렬화 및 역직렬화 메서드 구축

--- src/main/java/pairly/common/util/JsonConverter.java
+++ src/main/java/pairly/common/util/JsonConverter.java
@@ -1,4 +1,16 @@
 package pairly.common.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class JsonConverter {
+    private static final Gson GSON = new GsonBuilder().create();
+
+    public String toJson(Object object) {
+        return GSON.toJson(object);
+    }
+
+    public <T> T fromJson(String json, Class<T> clazz) {
+        return GSON.fromJson(json, clazz);
+    }
 }
